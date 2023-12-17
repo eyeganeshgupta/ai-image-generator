225632c39789f425136261ea1d30c4e843198a46
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-12-17 21:17:29 +0530

    image-generator-component

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -8,7 +8,37 @@ const ImageGenerator = () => {
 
   const [loading, setLoading] = useState(false);
 
-  
+  const imageGenerator = async () => {
+    if (inputRef.current.value === "") {
+      return 0;
+    }
+
+    setLoading(true);
+
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer api-key",
+          "User-Agent": "Chrome",
+        },
+        body: JSON.stringify({
+          prompt: `${inputRef.current.value}`,
+          n: 1,
+          size: "512x512",
+        }),
+      }
+    );
+
+    let data = await response.json();
+    console.log(data);
+    let dataArray = data?.data;
+    setImageUrl(dataArray[0].url);
+
+    setLoading(false);
+  };
 
   return (
     <div className="ai-image-generator">
